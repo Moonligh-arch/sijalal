4a8960d89be4c3e2ae9ea2a2ddbc130701b14de2
Alt <linggaaltha@gmail.com>
2026-06-05 18:06:04 +0700

    ALGO V4

--- projectalgo/MyForm.h
+++ projectalgo/MyForm.h
@@ -71,15 +71,22 @@ namespace projectalgo {
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::Label^ label5;
 	private: System::Windows::Forms::TextBox^ textBox4;
 	private: System::Windows::Forms::TextBox^ textBox5;
 	private: System::Windows::Forms::Label^ label6;
 	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Button^ button5;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Label^ label1;
+
+
+
+
+
+
 
 	private:
 		System::ComponentModel::Container^ components;
@@ -96,92 +103,100 @@ namespace projectalgo {
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
 			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label2
 			// 
+			this->label2->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label2->AutoSize = true;
 			this->label2->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->label2->Location = System::Drawing::Point(104, 289);
+			this->label2->Location = System::Drawing::Point(792, 364);
 			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(55, 20);
+			this->label2->Size = System::Drawing::Size(84, 29);
 			this->label2->TabIndex = 14;
 			this->label2->Text = L"jumlah";
 			// 
 			// label3
 			// 
+			this->label3->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label3->AutoSize = true;
 			this->label3->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->label3->Location = System::Drawing::Point(110, 319);
+			this->label3->Location = System::Drawing::Point(798, 394);
 			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(50, 20);
+			this->label3->Size = System::Drawing::Size(74, 29);
 			this->label3->TabIndex = 13;
 			this->label3->Text = L"harga";
 			// 
 			// label4
 			// 
+			this->label4->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label4->AutoSize = true;
 			this->label4->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->label4->Location = System::Drawing::Point(86, 203);
+			this->label4->Location = System::Drawing::Point(774, 278);
 			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(75, 20);
+			this->label4->Size = System::Drawing::Size(114, 29);
 			this->label4->TabIndex = 12;
 			this->label4->Text = L"id barang";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(177, 291);
+			this->textBox1->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->textBox1->Location = System::Drawing::Point(865, 366);
 			this->textBox1->Margin = System::Windows::Forms::Padding(2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(156, 20);
+			this->textBox1->Size = System::Drawing::Size(156, 26);
 			this->textBox1->TabIndex = 11;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(177, 228);
+			this->textBox2->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->textBox2->Location = System::Drawing::Point(865, 303);
 			this->textBox2->Margin = System::Windows::Forms::Padding(2);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(156, 20);
+			this->textBox2->Size = System::Drawing::Size(156, 26);
 			this->textBox2->TabIndex = 10;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(177, 320);
+			this->textBox3->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->textBox3->Location = System::Drawing::Point(865, 395);
 			this->textBox3->Margin = System::Windows::Forms::Padding(2);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(156, 20);
+			this->textBox3->Size = System::Drawing::Size(156, 26);
 			this->textBox3->TabIndex = 9;
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(64, 455);
+			this->dataGridView1->Location = System::Drawing::Point(750, 530);
 			this->dataGridView1->Margin = System::Windows::Forms::Padding(2);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->Size = System::Drawing::Size(380, 299);
+			this->dataGridView1->Size = System::Drawing::Size(396, 299);
 			this->dataGridView1->TabIndex = 8;
 			// 
 			// button1
 			// 
+			this->button1->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Bold));
-			this->button1->Location = System::Drawing::Point(69, 348);
+			this->button1->Location = System::Drawing::Point(757, 423);
 			this->button1->Margin = System::Windows::Forms::Padding(2);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(115, 46);
@@ -192,8 +207,9 @@ namespace projectalgo {
 			// 
 			// button2
 			// 
+			this->button2->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold));
-			this->button2->Location = System::Drawing::Point(69, 399);
+			this->button2->Location = System::Drawing::Point(757, 474);
 			this->button2->Margin = System::Windows::Forms::Padding(2);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(115, 46);
@@ -202,77 +218,85 @@ namespace projectalgo {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->BackColor = System::Drawing::SystemColors::ScrollBar;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 72, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(90, 27);
-			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(355, 110);
-			this->label1->TabIndex = 5;
-			this->label1->Text = L"SIJALAL";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
-			// 
 			// label5
 			// 
+			this->label5->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label5->AutoSize = true;
+			this->label5->Cursor = System::Windows::Forms::Cursors::IBeam;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 25.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Underline)),
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(68, 138);
+			this->label5->Location = System::Drawing::Point(746, 209);
 			this->label5->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(392, 42);
+			this->label5->Size = System::Drawing::Size(580, 62);
 			this->label5->TabIndex = 4;
 			this->label5->Text = L"Sistem Inventaris Jalaludin";
+			this->label5->TextAlign = System::Drawing::ContentAlignment::TopCenter;
 			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(177, 258);
+			this->textBox4->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->textBox4->Location = System::Drawing::Point(865, 333);
 			this->textBox4->Margin = System::Windows::Forms::Padding(2);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(156, 20);
+			this->textBox4->Size = System::Drawing::Size(156, 26);
 			this->textBox4->TabIndex = 3;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(177, 198);
+			this->textBox5->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->textBox5->Location = System::Drawing::Point(865, 273);
 			this->textBox5->Margin = System::Windows::Forms::Padding(2);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(156, 20);
+			this->textBox5->Size = System::Drawing::Size(156, 26);
 			this->textBox5->TabIndex = 2;
 			// 
 			// label6
 			// 
+			this->label6->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label6->AutoSize = true;
 			this->label6->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->label6->Location = System::Drawing::Point(61, 232);
+			this->label6->Location = System::Drawing::Point(749, 307);
 			this->label6->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(103, 20);
+			this->label6->Size = System::Drawing::Size(153, 29);
 			this->label6->TabIndex = 1;
 			this->label6->Text = L"nama barang";
 			// 
 			// label7
 			// 
+			this->label7->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->label7->AutoSize = true;
 			this->label7->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->label7->Location = System::Drawing::Point(95, 259);
+			this->label7->Location = System::Drawing::Point(783, 334);
 			this->label7->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(66, 20);
+			this->label7->Size = System::Drawing::Size(100, 29);
 			this->label7->TabIndex = 0;
 			this->label7->Text = L"kategori";
 			// 
+			// button5
+			// 
+			this->button5->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->button5->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold));
+			this->button5->Location = System::Drawing::Point(1065, 303);
+			this->button5->Margin = System::Windows::Forms::Padding(2);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(83, 49);
+			this->button5->TabIndex = 17;
+			this->button5->Text = L"button Update";
+			this->button5->UseVisualStyleBackColor = false;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
 			// button3
 			// 
+			this->button3->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold));
-			this->button3->Location = System::Drawing::Point(300, 344);
+			this->button3->Location = System::Drawing::Point(988, 419);
 			this->button3->Margin = System::Windows::Forms::Padding(2);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(145, 49);
@@ -283,10 +307,11 @@ namespace projectalgo {
 			// 
 			// button4
 			// 
+			this->button4->Anchor = System::Windows::Forms::AnchorStyles::Top;
 			this->button4->BackColor = System::Drawing::Color::Red;
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold));
 			this->button4->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->button4->Location = System::Drawing::Point(299, 397);
+			this->button4->Location = System::Drawing::Point(987, 472);
 			this->button4->Margin = System::Windows::Forms::Padding(2);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(145, 49);
@@ -295,24 +320,28 @@ namespace projectalgo {
 			this->button4->UseVisualStyleBackColor = false;
 			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
 			// 
-			// button5
+			// label1
 			// 
-			this->button5->BackColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold));
-			this->button5->Location = System::Drawing::Point(377, 228);
-			this->button5->Margin = System::Windows::Forms::Padding(2);
-			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(83, 49);
-			this->button5->TabIndex = 17;
-			this->button5->Text = L"button Update";
-			this->button5->UseVisualStyleBackColor = false;
-			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->label1->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Bell MT", 72, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(723, 54);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(628, 162);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"SIJALAL";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackColor = System::Drawing::SystemColors::Window;
-			this->ClientSize = System::Drawing::Size(536, 765);
+			this->ClientSize = System::Drawing::Size(1897, 912);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
@@ -527,5 +556,10 @@ private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e
 			MessageBox::Show("Barang dengan ID '" + idStr + "' kagak ditemuin di gudang!", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 		}
 	}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+
+
+
 };
 }
